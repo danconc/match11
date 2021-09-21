c6ff6f926a16b684de42742cb6807602d5ba8109
Dan Concepcion <danconc@gmail.com>
2021-09-21 12:36:45 -0400

    initial commit

---
+++ assets/main.js
@@ -0,0 +1,12 @@
+/*********************************************
+ * fn: main.js
+ * by: danConcepcion
+ * dt: 20210916@1727
+ * co: Indy Attic >> Match 11 Game
+ * pn: Proof of Concept
+ *********************************************/
+
+
+$(
+    alert('Ready player one...');
+);
---
+++ assets/match11.js
@@ -0,0 +1,7 @@
+/*********************************************
+ * fn: match11.js
+ * by: danConcepcion
+ * dt: 20210916@1727
+ * co: Indy Attic >> Match 11 Game
+ * pn: Proof of Concept
+ *********************************************/
